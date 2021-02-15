45290242c50f0e4857c5ecaca5007276ee57cc08
westondavidson <weston@westondavidson.com>
2021-02-15 18:40:26 -0500

    Completed Challenge 2 program

---
+++ Microsoft-Docs-Challenge-2/challenge2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace challenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Bob";
+            int emails = 3;
+            decimal temperature = 34.4m;
+
+            Console.WriteLine($"Hello {name}! You have {emails} in your inbox. The temperature is {temperature} celcius");
+
+
+        }
+    }
+}
